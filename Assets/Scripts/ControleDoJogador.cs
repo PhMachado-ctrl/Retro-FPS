21d73b8738871df1447b35b880d4b45a41a9ed73
PhMachado-ctrl <phmachado555@gmail.com>
2025-03-16 22:50:10 -0300

    Movimento do Mouse rotacionar personagem

--- Assets/Scripts/ControleDoJogador.cs
+++ Assets/Scripts/ControleDoJogador.cs
@@ -8,7 +8,11 @@ public class ControleDoJogador : MonoBehaviour
     
     public float velocidadeDoJogador; //Armazena a velocidade de movimento do jogador como um valor flutuante.
 
+    public float sensibilidadeDoMouse; // Velocidade da Movimentação do mouse
+
     private Vector2 comandosDoJogador; //Armazena o valor X e Y
+
+    private Vector2 movimentoDoMouse; //Armazena os valores de posição do Mouse
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +24,7 @@ public class ControleDoJogador : MonoBehaviour
     void Update()
     {
         MovimentarJogador();
+        GirarJogador();
     }
 
     //Metodo de Movimento do jogador
@@ -31,10 +36,19 @@ public class ControleDoJogador : MonoBehaviour
 
         // Variaveis tipo Vector 3 armazenam Posições X, Y, Z
         // Transform.up serve para acessar o eixo que vai para cima que visão do personagem é para esquerda e direita
-        Vector3 movimentoHorizontal = transform.up * comandosDoJogador.x;
+        Vector3 movimentoHorizontal = transform.up * -comandosDoJogador.x;
         // Transform.up serve para acessar o eixo que vai para direito que visão do personagem é para cima e para Baixo
         Vector3 movimentoVertical = transform.right * comandosDoJogador.y;
-        //Aplica velocidade ao corpo do Player
+        //Aplica velocidade ao corpo do Player, soma a posição x e y vezes a velocidade
         oRigibody2D.velocity = (movimentoHorizontal + movimentoVertical) * velocidadeDoJogador;
+    }
+
+    private void GirarJogador()
+    {
+        //Coleta os Inputs sem muita precisão da posição X e Y do mouse.
+        movimentoDoMouse = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y") * sensibilidadeDoMouse);
+        //Rodação do transform do jogador
+        // Quaternion usa 4 eixos X Y Z W e o Euler converte em 3 eixos
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z - movimentoDoMouse.x);
     }
 }
