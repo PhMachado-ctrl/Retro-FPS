f03e8c9de10ff9edebac0d03ce3dd779e997341e
PhMachado-ctrl <phmachado555@gmail.com>
2025-03-23 18:44:16 -0300

    Script inimigo | Pontos Caminhar

---
+++ Assets/Scripts/Inimigo.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inimigo : MonoBehaviour
+{
+    public float velocidadeInimigo;
+    public Transform[] PontosParaCaminhar;
+    public int pontoAtual;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
