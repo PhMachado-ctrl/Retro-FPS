e02b2f8147fde559361d6e5c570372cb4d4f499d
PhMachado-ctrl <phmachado555@gmail.com>
2025-04-11 23:59:53 -0300

    Texto de vida na tela

--- Assets/Scripts/VidaDoJogador.cs
+++ Assets/Scripts/VidaDoJogador.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VidaDoJogador : MonoBehaviour
 {
     public int vidaMaxima;
     public int vidaAtual;
+
+    public Text textoVidaJogador;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        textoVidaJogador.text = "VIDA\n" + vidaAtual;
     }
 
     // Update is called once per frame
@@ -23,7 +27,7 @@ public class VidaDoJogador : MonoBehaviour
         if (GameManager.instance.JogadorEstaVivo == true)
         {
             vidaAtual -= danoRecebido;
-
+            textoVidaJogador.text = "VIDA\n" + vidaAtual;
             if (vidaAtual <= 0)
             {
                 GameManager.instance.GameOver();
